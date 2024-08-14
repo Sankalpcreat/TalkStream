b19e4f543bb951eb2b9442cb0372b19bec1a0fa4
Sankalp Singh <singhsankalp997@gmail.com>
2024-08-14 09:10:12 +0530

    first-commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+
+const authConfig= {
+    providers: [
+      {
+        domain: "https://sacred-yeti-95.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  export default authConfig;
